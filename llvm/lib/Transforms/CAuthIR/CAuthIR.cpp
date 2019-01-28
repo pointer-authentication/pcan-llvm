8b4beef3acd5e5d3afd448add2f2b3af1721e107
Zaheer <zaheer.gauhar@aalto.fi>
2019-01-28 16:53:16 +0200

    Cauth IR pass for adding intrinsics

--- llvm/lib/Transforms/CAuthIR/CAuthIR.cpp
+++ llvm/lib/Transforms/CAuthIR/CAuthIR.cpp
@@ -25,7 +25,7 @@
 using namespace llvm;
 using namespace CAUTH;
 
-#define DEBUG_TYPE "cauth-ir"
+#define DEBUG_TYPE "\ncauth-ir:\t"
 
 
 
@@ -46,32 +46,51 @@ namespace {
       unsigned numBuffs = 0;
       Value* oldcbuff = nullptr;
       Instruction *loc = nullptr;
+      auto &C = F.getParent()->getContext();
+      Type* int64Ty = Type::getInt64Ty(C);
+      Value *pacga_instr = nullptr;
       for (auto &BB : F){
         for (auto &I : BB){
-          errs() << DEBUG_TYPE;
-          
+          //errs() << DEBUG_TYPE;
           if(isa<AllocaInst>(I)){
             llvm::AllocaInst* aI = dyn_cast<llvm::AllocaInst>(&I);
-            
             if(I.getName().find("cauth_alloc") == std::string::npos){
-               auto &C = F.getParent()->getContext();
-              Type* int64Ty = Type::getInt64Ty(C);
               auto num = 1;
               ArrayType* arrayType = ArrayType::get(int64Ty, num);
               loc = I.getNextNode();
               AllocaInst* arr_alloc = new AllocaInst(int64Ty , 0, "cauth_alloc", loc);
-              oldcbuff = arr_alloc;
               ++numBuffs;
-              Value* pac_instr = nullptr;
-              if (numBuffs==1)
-                pac_instr = CauthIntr::pacga(F, *loc);
-              else if (numBuffs>1)
-                pac_instr = CauthIntr::pacda(F, *loc, oldcbuff);
-              //auto store = new StoreInst(paced_instr, arr_alloc, loc);
+              
+              IRBuilder<> Builder(loc);
+              if (numBuffs==1){
+                pacga_instr = CauthIntr::pacga(F, *loc);
+                Builder.CreateStore(pacga_instr, arr_alloc);
+                oldcbuff = llvm::cast<llvm::Value>(arr_alloc);
+              }
+              else if (numBuffs>1){
+                pacga_instr = CauthIntr::pacda(F, *loc, oldcbuff);
+                oldcbuff = llvm::cast<llvm::Value>(arr_alloc);
+                auto paced_val = new PtrToIntInst(pacga_instr, int64Ty, "paced_val", loc);
+                Builder.CreateStore(paced_val, oldcbuff);
+              }
             }
           }
-          else if(isa<ReturnInst>(I)){
-            auto aut_instr = CauthIntr::autda(F, I, oldcbuff);
+          else if(isa<ReturnInst>(I) && numBuffs>0){
+            IRBuilder<> Builder(&I);
+            auto canary_val = Builder.CreateLoad(oldcbuff);
+            for (int i=numBuffs; i>0; i--){
+              if (i == 1){
+                auto pacga2_instr = CauthIntr::pacga(F, I);
+                auto cmp = new ICmpInst(&I, llvm::ICmpInst::getSignedPredicate(llvm::CmpInst::ICMP_EQ), canary_val, pacga2_instr);
+              }
+              else{
+              
+              Type* ty=pacga_instr->getType();
+              auto aut_val = new IntToPtrInst(canary_val, ty, "", &I);
+              Value* autda_instr = CauthIntr::autda(F, I, aut_val);
+              canary_val = Builder.CreateLoad(autda_instr);
+              }
+            }
           }
         }
          BB.dump();
