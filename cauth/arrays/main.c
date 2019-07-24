f0d4747b4dc8a69fd1c7f815b6f8c58e2997c4a0
Hans Liljestrand <hans.liljestrand@aalto.fi>
2019-07-24 16:33:23 +0300

    cauth: Add some separate tests

---
+++ cauth/arrays/main.c
@@ -0,0 +1,53 @@
+/*
+ * Author: Hans Liljestrand <hans.liljestrand@aalto.fi>
+ * Copyright: Secure Systems Group, Aalto University, https://ssg.aalto.fi
+ *
+ * This code is released under Apache 2.0 license
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+__attribute__((noinline))
+void do_copy(char * dst, const char *src) {
+    size_t len = strlen(src);
+    strncpy(dst, src, len);
+}
+
+__attribute__((noinline))
+void tester(void) {
+    char a1[32] = { 0 };
+    char a2[64] = { 0 };
+    char a3[128] = { 0 };
+    char a4[256] = { 0 };
+
+    char mul[4][32] = {
+        { 0 },
+        { 0 },
+        { 0 },
+        { 0 }
+    };
+
+    do_copy(a1, "Hello World");
+    do_copy(a2, "Hello World");
+    do_copy(a3, "Hello World");
+    do_copy(a4, "Hello World");
+
+    for (int i = 0; i < 4; ++i) {
+        do_copy(mul[i], "Hello World");
+    }
+
+    printf("%s\n", a1);
+    printf("%s\n", a2);
+    printf("%s\n", a3);
+    printf("%s\n", a4);
+
+    for (int i = 0; i < 4; ++i) {
+        printf("%s\n", mul[i]);
+    }
+}
+
+int main(void) {
+    tester();
+    return 0;
+}
---
+++ cauth/run_all.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+#
+# Author: Hans Liljestrand <hans.liljestrand@aalto.fi>
+# Copyright: Secure Systems Group, Aalto University, https://ssg.aalto.fi
+#
+# This code is released under Apache 2.0 license
+#
+
+tests="arrays"
+fails="hack01"
+xfails=""
+
+FAILED=""
+XFAILS=""
+XFAILSOK=""
+MISSING=""
+
+run_test() {
+    if [ -e "./$1" ]; then
+        if [ -n "$2" ]; then
+            if "./$1" "$2" > /dev/null 2>&1; then
+                echo "$1 $2... OKAY!"
+            else
+                echo "$1 $2... FAILED!"
+                FAILED="$FAILED\n\t$1 $2"
+            fi
+        else
+            if "./$1" > /dev/null 2>&1; then
+                echo "$1... OKAY!"
+            else
+                echo "$1... FAILED!"
+                FAILED="$FAILED\n\t$1 "
+            fi
+        fi
+    else
+        echo "$1... MISSING!"
+        MISSING="$MISSING\n\t$1* "
+    fi
+}
+
+run_xfail() {
+    if [ -e "./$1" ]; then
+        if [ -n "$2" ]; then
+            if "./$1" "$2" > /dev/null 2>&1; then
+                echo "$1 $2... UNEXPECTED OKAY!"
+                XFAILSOK="$XFAILSOK\n\t$1 $2"
+            else
+                echo "$1 $2... FAILED"
+                XFAILS="$XFAILS\n\t$1 $2"
+            fi
+        else
+            if "./$1" > /dev/null 2>&1; then
+                echo "$1... UNEXPECTED OKAY!"
+                XFAILSOK="$XFAILSOK\n\t$1 "
+            else
+                echo "$1... FAILED"
+                XFAILS="$XFAILS\n\t$1 "
+            fi
+        fi
+    else
+        echo "$1... MISSING!"
+        MISSING="$MISSING\n\t$1* "
+    fi
+}
+
+run_fail_test() {
+    if [ -e "./$1" ]; then
+        if [ -n "$2" ]; then
+            if "./$1" "$2" > /dev/null 2>&1; then
+                echo "$1 $2... FAILED TO FAIL!"
+                FAILED="$FAILED\n\t!$1 $2"
+            else
+                echo "$1 $2... OKAY!"
+            fi
+        else
+            if "./$1" > /dev/null 2>&1; then
+                echo "$1 $2... FAILED TO FAIL!"
+                FAILED="$FAILED\n\t!$1 "
+            else
+                echo "$1... OKAY!"
+            fi
+        fi
+    else
+        echo "$1... MISSING!"
+        MISSING="$MISSING\n\t$1* "
+    fi
+}
+
+for test in $fails; do
+	run_fail_test $test.out
+done
+
+for test in $tests; do
+	run_test $test.out
+done
+
+for test in $xfails; do
+	run_xfail $test.out
+done
+
+if [ ! -z "$XFAILS" ]; then
+    echo ""
+    echo "  expected failures"
+    echo "  -----------------"
+    echo -e "$XFAILS"
+fi
+
+if [ ! -z "$MISSING" ]; then
+    echo ""
+    echo "  #######################"
+    echo "  # MISSING INPUT FILES #"
+    echo "  #######################"
+    echo ""
+    echo -e "$MISSING"
+fi
+
+if [ ! -z "$XFAILSOK" ]; then
+    echo ""
+    echo "  ######################"
+    echo "  # UNEXPECTED SUCCESS #"
+    echo "  ######################"
+    echo ""
+    echo -e "$XFAILSOK"
+fi
+
+if [ ! -z "$FAILED" ]; then
+    echo ""
+    echo "  #####################"
+    echo "  # FAILED SOME TESTS #"
+    echo "  #####################"
+    echo ""
+    echo -e "$FAILED"
+fi
+
